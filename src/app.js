7827dfca67a9775afa1d304bd83dccbec7e49a1c
Amir Sanni <amirsanni@gmail.com>
2019-12-30 10:39:48 +0100

    Remove chat namespace

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ let express = require('express');
 let app = express();
 let server = require('http').Server(app);
 let io = require('socket.io')(server);
-let chat = require('./ws/chat');
 let stream = require('./ws/stream')
 
 
@@ -10,7 +9,6 @@ app.get('/', (req, res)=>{
     res.sendFile(__dirname+'/index.html');
 });
 
-io.of('/chat').on('connection', chat);
 io.of('/stream').on('connection', stream);
 
 server.listen(3000);
