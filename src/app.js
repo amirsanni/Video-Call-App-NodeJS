ea4398de44011c5d2cd88f43f148c2f0ae770661
Amir Sanni <amirsanni@gmail.com>
2020-01-06 16:47:33 +0100

    Code refactor

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ let express = require('express');
 let app = express();
 let server = require('http').Server(app);
 let io = require('socket.io')(server);
-let stream = require('./ws/stream')
+let stream = require('./ws/stream');
+let path = require('path');
 
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname+'/index.html');
