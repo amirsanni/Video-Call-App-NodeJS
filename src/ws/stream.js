40721dfc6cf4da8f49c6f567b713bdb55e9a225f
Amir Sanni <amirsanni@gmail.com>
2020-01-06 12:55:49 +0100

    Debug

--- src/ws/stream.js
+++ src/ws/stream.js
@@ -2,7 +2,7 @@ const stream = (socket)=>{
     socket.on('subscribe', (data)=>{
         //subscribe/join a room
         socket.join(data.room);
-        socket.join(data.username);
+        // socket.join(data.username);
 
         //Inform other members in the room of new user's arrival
         if(socket.adapter.rooms[data.room].length > 1){
