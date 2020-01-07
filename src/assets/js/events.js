1578145fb673e0edef05bf7242a32a09d13fc658
Amir Sanni <amirsanni@gmail.com>
2020-01-07 14:42:06 +0100

    Add PIP feature

--- src/assets/js/events.js
+++ src/assets/js/events.js
@@ -2,4 +2,23 @@ window.addEventListener('load', ()=>{
     document.querySelector('#toggle-chat-pane').addEventListener('click', (e)=>{
         document.querySelector('#chat-pane').classList.toggle('chat-opened');
     });
+
+
+    document.getElementById('local').addEventListener('click', ()=>{
+        if (!document.pictureInPictureElement) {
+            document.getElementById('local').requestPictureInPicture()
+            .catch(error => {
+                // Video failed to enter Picture-in-Picture mode.
+                console.error(error);
+            });
+        } 
+          
+        else {
+            document.exitPictureInPicture()
+            .catch(error => {
+                // Video failed to leave Picture-in-Picture mode.
+                console.error(error);
+            });
+        }
+    });
 })
