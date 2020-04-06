c29a8fd443e6da8ad98b83edca409416bc6b0fdb
Amir Sanni <amirsanni@gmail.com>
2020-04-06 10:54:27 +0100

    Install server-favicon

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ let stream = require('./ws/stream');
 let path = require('path');
 let favicon = require('serve-favicon')
 
-app.use(favicon(path.join(__dirname, '', 'favicon.ico')))
+app.use(favicon(path.join(__dirname, 'favicon.ico')))
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res)=>{
