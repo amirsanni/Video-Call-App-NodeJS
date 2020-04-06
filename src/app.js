357d2d8bfe57df216120775bf4b9751d04b473fc
Amir Sanni <amirsanni@gmail.com>
2020-04-06 10:52:23 +0100

    Add favicon.ico

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ let server = require('http').Server(app);
 let io = require('socket.io')(server);
 let stream = require('./ws/stream');
 let path = require('path');
+let favicon = require('serve-favicon')
 
+app.use(favicon(path.join(__dirname, '', 'favicon.ico')))
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res)=>{
