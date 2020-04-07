60313c4e3c87e021b85474cfda9383d26f600b35
Amir Sanni <amirsanni@gmail.com>
2020-04-07 22:19:28 +0100

    Fix bug in adding remote stream video control

--- src/assets/js/rtc.js
+++ src/assets/js/rtc.js
@@ -202,8 +202,8 @@ window.addEventListener('load', ()=>{
                     //video controls elements
                     let controlDiv = document.createElement('div');
                     controlDiv.className = 'remote-video-controls';
-                    controlDiv.appendChild('<i class="fa fa-microphone text-white pr-3 mute-remote-mic" title="Mute"></i>');
-                    controlDiv.appendChild('<i class="fa fa-expand text-white expand-remote-video" title="Expand"></i>');
+                    controlDiv.innerHTML = `<i class="fa fa-microphone text-white pr-3 mute-remote-mic" title="Mute"></i>
+                        <i class="fa fa-expand text-white expand-remote-video" title="Expand"></i>`;
                     
                     //create a new div for card
                     let cardDiv = document.createElement('div');
