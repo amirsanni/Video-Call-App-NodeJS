3c112b5e343bdcbe1ec9adc5fd6ed441ae0870a4
Amir Sanni <amirsanni@gmail.com>
2020-04-11 20:11:26 +0100

    Remove one turn server due to warning in firefox

--- src/assets/js/helpers.js
+++ src/assets/js/helpers.js
@@ -104,7 +104,8 @@ export default {
 
 
     getIceServer(){
-        //"turns:eu-turn4.xirsys.com:5349?transport=tcp"
+        // "turns:eu-turn4.xirsys.com:5349?transport=tcp"
+        // "turns:eu-turn4.xirsys.com:443?transport=tcp"
         return {
             iceServers: [
                 {
@@ -117,8 +118,7 @@ export default {
                         "turn:eu-turn4.xirsys.com:80?transport=udp", 
                         "turn:eu-turn4.xirsys.com:3478?transport=udp",
                         "turn:eu-turn4.xirsys.com:80?transport=tcp",
-                        "turn:eu-turn4.xirsys.com:3478?transport=tcp",
-                        "turns:eu-turn4.xirsys.com:443?transport=tcp"
+                        "turn:eu-turn4.xirsys.com:3478?transport=tcp"
                     ]
                 }
             ]
