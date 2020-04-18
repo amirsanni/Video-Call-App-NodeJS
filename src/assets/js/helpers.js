7ffb1b5e8d0db2fa9d9f5bec9e4d417bc675599b
Amir Sanni <amirsanni@gmail.com>
2020-04-18 23:39:05 +0100

    Auto-adjust video elem size based on number of users

--- src/assets/js/helpers.js
+++ src/assets/js/helpers.js
@@ -7,6 +7,7 @@ export default {
     closeVideo(elemId){
         if(document.getElementById(elemId)){
             document.getElementById(elemId).remove();
+            this.adjustVideoElemSize();
         }
     },
 
@@ -275,5 +276,68 @@ export default {
         
         localVidElem.srcObject = stream;
         mirrorMode ? localVidElem.classList.add('mirror-mode') : localVidElem.classList.remove('mirror-mode');
+    },
+
+
+    adjustVideoElemSize(){
+        let elem = document.getElementsByClassName('card');
+        let totalRemoteVideosDesktop = elem.length;
+        let newWidth = totalRemoteVideosDesktop == 1 ? '100%' : (
+            totalRemoteVideosDesktop == 2 ? '50%' : (
+                totalRemoteVideosDesktop == 3 ? '33.33%' : (
+                    totalRemoteVideosDesktop <= 8 ? '25%' : (
+                        totalRemoteVideosDesktop <= 15 ? '20%' : (
+                            totalRemoteVideosDesktop <= 18 ? '16%' : (
+                                totalRemoteVideosDesktop <= 23 ? '15%' : (
+                                    totalRemoteVideosDesktop <= 32 ? '12%' : '10%'
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        );
+
+
+        for(let i = 0; i < totalRemoteVideosDesktop; i++){
+            elem[i].style.width = newWidth;
+        }
+    },
+
+
+    createDemoRemotes(str, total=6){
+        let i = 0;
+
+        let testInterval = setInterval(()=>{
+            let newVid = document.createElement('video');
+            newVid.id = `demo-${i}-video`;            
+            newVid.srcObject = str;
+            newVid.autoplay = true;
+            newVid.className = 'remote-video';
+
+            //video controls elements
+            let controlDiv = document.createElement('div');
+            controlDiv.className = 'remote-video-controls';
+            controlDiv.innerHTML = `<i class="fa fa-microphone text-white pr-3 mute-remote-mic" title="Mute"></i>
+                <i class="fa fa-expand text-white expand-remote-video" title="Expand"></i>`;
+            
+            //create a new div for card
+            let cardDiv = document.createElement('div');
+            cardDiv.className = 'card card-sm';
+            cardDiv.id = `demo-${i}`;
+            cardDiv.appendChild(newVid);
+            cardDiv.appendChild(controlDiv);
+            
+            //put div in main-section elem
+            document.getElementById('videos').appendChild(cardDiv);
+
+            this.adjustVideoElemSize();
+
+            i++;
+
+            if(i == total){
+                clearInterval(testInterval);
+            }
+        }, 2000);
     }
 };
